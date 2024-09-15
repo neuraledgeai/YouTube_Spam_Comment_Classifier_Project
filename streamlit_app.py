887d8e942042e468c98c7401c609f17fb93816f8
Neural Edge AI <64458507+neuraledgeai@users.noreply.github.com>
2024-09-15 19:04:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,7 +66,10 @@ def make_prediction(raw_text, threshold=0.8):
 if comment.strip():
     # Make the prediction
     result = make_prediction(raw_text = comment, threshold = threshold)
-    if result == "Not Spam" st.success(f"This comment is {result}") else st.error(f"This comment is {result}")
+    if result == "Not Spam":
+    st.success(f"This comment is {result}")
+    else:
+    st.error(f"This comment is {result}")
     #st.success(f"This comment is {result}")
 else:
     st.warning("Please enter a YouTube comment.")
